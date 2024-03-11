7b548639ee9dbc6aeb967f5d52d8f589b1daa004
Fikri Alfan Fasha <elfankiki@gmail.com>
2024-03-11 20:51:50 +0700

    first

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Fikri Alfan F | Portofolio';
+export const SITE_DESCRIPTION = 'Astro is an all in one web framework';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
